b42753f724b84416170c082cb68480a2eb32a92b
Mehmet Akif Yanbak <makifyanbak@gmail.com>
2016-01-19 21:40:29 +0200

    derleme hatası

--- Ajax.BeginFormSample/Ajax.BeginFormSample/Properties/AssemblyInfo.cs
+++ Ajax.BeginFormSample/Ajax.BeginFormSample/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ajax.BeginFormSample")]
+[assembly: AssemblyTitle("Jquery.Ajax.FileUpload")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ajax.BeginFormSample")]
+[assembly: AssemblyProduct("Jquery.Ajax.FileUpload")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
